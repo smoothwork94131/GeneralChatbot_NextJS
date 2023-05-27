f02e9792eb5770199ab9af307885b267a454d4b7
smoothwork94131 <developeralex1994131@gmail.com>
2023-05-27 06:50:43 +0200

    fixed header style

--- components/Common/Layouts.tsx
+++
@@ -1,9 +0,0 @@
-import AccountLayout from "@/components/Account/Layout";
-import MainLayout from "./MainLayout";
-
-export const Layouts = {
-  Account: AccountLayout,
-  Main: MainLayout
-};
-export type LayoutKeys = keyof typeof Layouts; // "Main" | "Admin"
-
--- components/Common/MainLayout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from "react";
-const AdminLayout = (props: PropsWithChildren) => {
-  return (
-    <>
-      {props.children}
-    </>
-  );
-};
-export default AdminLayout;

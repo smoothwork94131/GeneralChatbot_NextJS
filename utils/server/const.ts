c54a476c319f06d000404cff4e7663eb6539e94a
smoothwork94131 <developeralex1994131@gmail.com>
2023-10-08 19:33:00 +0200

    fixed const security issues

--- utils/server/const.ts
+++ utils/server/const.ts
@@ -3,6 +3,6 @@ export const SHEET_RANGE = process.env.SHEET_RANGE || '';
 export const GOOGLE_SHEETS_CLIENT_EMAIL = process.env.GOOGLE_SHEETS_CLIENT_EMAIL || '';
 export const GOOGLE_SHEETS_PRIVATE_KEY = process.env.GOOGLE_SHEETS_PRIVATE_KEY || '';
 
-export const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqYnhtaGp0Y2lraXJsbHpuanFkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NDg3ODQwNywiZXhwIjoyMDAwNDU0NDA3fQ.IrT997yOpWsY6tEYdmq04oXHpMX0WFq26o1bKsJK3kU';
+export const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
 
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY || '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';
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY || '';
